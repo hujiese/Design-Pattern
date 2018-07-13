5aba00ddfce2bd3ecacc427a56aed934aa18320b
hujiese <jackster@163.com>
2018-07-13 18:37:54 +0800

    加入加单工厂

---
+++ 02.创建型模式/02.工厂模式/01.简单工厂/c++/FactoryPatternDemo.cpp
@@ -0,0 +1,32 @@
+#include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
+#include "Rectangle.h"
+#include "ShapeFactory.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	ShapeFactory* shapeFactory = new ShapeFactory();
+
+	//获取 Circle 的对象，并调用它的 draw 方法
+	Shape* shape1 = shapeFactory->getShape("CIRCLE");
+
+	//调用 Circle 的 draw 方法
+	shape1->draw();
+
+	//获取 Rectangle 的对象，并调用它的 draw 方法
+	Shape* shape2 = shapeFactory->getShape("RECTANGLE");
+
+	//调用 Rectangle 的 draw 方法
+	shape2->draw();
+
+	//获取 Square 的对象，并调用它的 draw 方法
+	Shape* shape3 = shapeFactory->getShape("SQUARE");
+
+	//调用 Square 的 draw 方法
+	shape3->draw();
+	return 0;
+}
