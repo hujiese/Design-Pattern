dd5320229d6471d35b16b6b2a602227cd2c5ef12
hujiese <jackster@163.com>
2019-07-23 10:39:28 +0800

    使用智能指针改写简单工厂模式

--- 02.创建型模式/02.工厂模式/01.简单工厂/c++/FactoryPatternDemo.cpp
+++ 02.创建型模式/02.工厂模式/01.简单工厂/c++/FactoryPatternDemo.cpp
@@ -5,34 +5,30 @@
 #include "ShapeFactory.h"
 #include <iostream>
 #include <string>
+#include <memory>
 using namespace std;
 
 int main(void)
 {
-	ShapeFactory* shapeFactory = new ShapeFactory();
+	shared_ptr<ShapeFactory> shapeFactory(new ShapeFactory);
 
 	//获取 Circle 的对象，并调用它的 draw 方法
-	Shape* shape1 = shapeFactory->getShape("CIRCLE");
+	shared_ptr<Shape> shape1(shapeFactory->getShape("CIRCLE"));
 
 	//调用 Circle 的 draw 方法
 	shape1->draw();
 
 	//获取 Rectangle 的对象，并调用它的 draw 方法
-	Shape* shape2 = shapeFactory->getShape("RECTANGLE");
+	shared_ptr<Shape> shape2(shapeFactory->getShape("RECTANGLE"));
 
 	//调用 Rectangle 的 draw 方法
 	shape2->draw();
 
 	//获取 Square 的对象，并调用它的 draw 方法
-	Shape* shape3 = shapeFactory->getShape("SQUARE");
+	shared_ptr<Shape> shape3(shapeFactory->getShape("SQUARE"));
 
 	//调用 Square 的 draw 方法
 	shape3->draw();
-
-	delete shapeFactory;
-	delete shape1;
-	delete shape2;
-	delete shape3;
 
 	return 0;
 }
